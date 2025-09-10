48f9d0a75e60d26167dd650c16bee2d9ec24e923
Abdul Ghani <abdulghani4920@gmail.com>
2025-09-10 07:20:23 -0700

    Issue Solved

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE-BASE-PATH || "/Quiz-App-React-Project"
 })
